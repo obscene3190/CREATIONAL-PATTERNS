5253fe6b4cedad0a3bc4a317d759625665ef3952
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-05-02 21:32:41 +0300

    Update Фабричный метод[IN PROCESS].cpp

--- Фабричный метод[IN PROCESS].cpp
+++ Фабричный метод[IN PROCESS].cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 using namespace std;
 
+//Задача:
+//Создание фабрики, на которой можно собирать три вида транспорта: автомобили, лодки и самолеты
+
+
+//Общий интерфейс объектов, которые будет создавать наша фабрика. Так как это транспорт, то он имеет сидения и вес(использовать их не будем)
 class Transport {
 public:
     int seats;
     int weight;
-    virtual void info() = 0;
+    virtual void info() = 0; //Виртуальная функция, которая будет предоставлять информацию о виде транспорта, у каждого своя
 };
 
+//Создаем наши объекты ,которые наследуют общий интерфейс и переопределяют нашу виртуальную функцию
+//Отличительной чертой машины будут колеса, лодки - весла и самолета - пропеллеры.
+//Чтобы в дальнейшем увидеть различие, добавлен метод info, который будет выводить особенности каждого типа транспорта
 class Car:public Transport {
 public:
     int wheels = 4;
-
     void info() {
         cout<<"Car with "<<wheels<<" wheels"<<endl;
 
@@ -21,7 +28,6 @@ public:
 class Boat:public Transport {
 public:
     int paddles = 2;
-
     void info() {
         cout<<"Boat with "<<paddles<<" paddles"<<endl;
     }
@@ -30,19 +36,20 @@ public:
 class Plane:public Transport {
 public:
     int propellers = 2;
-
     void info() {
         cout<<"Plane with "<<propellers<<" propellers"<<endl;
 
     }
 };
 
+//Создаем класс общей "руководящей" фабрики, которая имеет виртуальный метод create, переопределяемый для каждой подфабрики
 class Factory{
 public:
     virtual Transport* create() = 0;
     virtual ~Factory(){}
 };
 
+//Создаем производство каждого отдельного типа
 class CarFactory:public Factory {
 public:
     Transport* create() {
@@ -64,19 +71,27 @@ public:
     }
 };
 
+//Теперь можно создать функцию сборки, которая будет принимать фабрику и собирать нам транспорт, не зависимо от его типа
 Transport* Assembling(Factory *value) {
     return value->create();
 }
 
+
 int main(){
-    CarFactory car;
-    Factory *factory1 = &car;
-    Transport *transport1 = Assembling(factory1);
-    transport1->info();
+    
+    CarFactory car;//Создаем переменные каждого подкласса
+    Factory *factory1 = &car;//Создаем указатели на объекты нужных типов
+    Transport *transport1 = Assembling(factory1);//Создаем новый траспорт и собираем его на фабрике
+    transport1->info();//Смотрим,что за объект мы собрали
+    //Далее для каждого подкласса
+    
+    
     BoatFactory boat;
     Factory *factory2 = &boat;
     Transport *transport2 = Assembling(factory2);
     transport2->info();
+    
+    
     PlaneFactory plane;
     Factory *factory3 = &plane;
     Transport *transport3 = Assembling(factory3);
