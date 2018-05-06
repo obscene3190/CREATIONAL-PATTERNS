b73346c55fdc57ac94314cd9de16f647971c723a
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-05-06 22:52:14 +0300

    Update Прототип.cpp

--- Прототип.cpp
+++ Прототип.cpp
@@ -8,31 +8,41 @@ public:
     int X;
     int Y;
     string color;
+    Shape(Shape const & other) {
+        X = other.X;
+        Y = other.Y;
+        color = other.color;
+    }
     virtual Shape* clone() = 0;
     virtual void info() = 0;
-    /*Shape(Shape &source) {
-            X = source.X;
-            Y = source.Y;
-            color = source.color;
-    }*/
+    /*static Shape * CreateShape(){
+        
+    } */
     
 };
-// Конкретный прототип. Метод клонирования создаёт новый объект
-// текущего класса, передавая в его конструктор ссылку на
-// собственный объект. Благодаря этому операция клонирования
-// получается атомарной — пока не выполнится конструктор, нового
-// объекта ещё не существует. Но как только конструктор завершит
-// работу, мы получим полностью готовый объект-клон, а не пустой
-// объект, который нужно ещё заполнить.
-class Rectangle : public Shape {
-    int width;
+
+// Конкретные прототипы
+class Rectangle : Shape {
     int height;
-    Rectangle(Rectangle & source){ 
-        // Вызов родительского конструктора нужен, чтобы
-        // скопировать потенциальные приватные поля, объявленные
-        // в родительском классе.
-        //super(source)
-            width = source.width;
-            height = source.height;
-        }
+    int weight;
+    Rectangle(Rectangle const & other) : Shape(other) {
+        height = other.height;
+        weight = other.weight;
+    }
+    Shape* clone() { 
+        return new Rectangle(*this);
+    }
+    void info() {
+        cout<<"Rectangle"<<endl;
+    }
 };
+
+class Circle : public Shape {
+    Shape* clone() {
+        return new Circle(*this);
+    }
+    void info() {
+        cout<<"Circle"<<endl;
+    }
+};
+
