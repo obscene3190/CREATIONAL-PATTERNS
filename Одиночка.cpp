0811fe6dbed9188da9c8f50be9d399cc2a9596d1
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-05-10 21:32:16 +0300

    Create Одиночка.cpp

---
+++ Одиночка.cpp
@@ -0,0 +1,84 @@
+
+//Классическая реализация
+//Недостаток: придется вручную удалять наш объект
+class Singleton1
+{
+  private:
+    static Singleton1 * one;
+    Singleton1() {}
+    Singleton1( const Singleton1& );  
+    Singleton1& operator=( Singleton1& );
+  public:
+    static Singleton1 * getNew() {
+        if(!one)           
+            one = new Singleton1();
+        return one;
+    }
+};
+
+//Реализация, предложенная Скоттом Мэйерсом(Scott Meyers)
+//Объект сам уничтожится после завершения программы, тк он статический
+//Недостатки: сложности создания объектов производных классов и невозможность безопасного доступа 
+//нескольких клиентов к единственному объекту в многопоточной среде.
+class SingletonScott
+{
+private: 
+    SingletonScott() {}
+    SingletonScott( const SingletonScott&);  
+    SingletonScott& operator=( SingletonScott& );
+public:
+    static SingletonScott& getNew() {
+        static SingletonScott  one;
+        return one;
+    }    
+}; 
+
+//Улучшенная реализация
+//Добавлен специальный метод для автоматического удаления
+//
+//
+class Singleton;
+  
+class SingletonDestroyer
+{
+  private:
+    Singleton* p_instance; //это переменная, которая будет удаляться и в которую будет перезаписываться наша
+  public:    
+    ~SingletonDestroyer(); //удаляет переменную
+    void initialize( Singleton* p );//переписывает аргумент в наш объект
+};
+  
+class Singleton
+{
+  private:
+    static Singleton* p_instance;
+    static SingletonDestroyer destroyer;
+  protected: 
+    Singleton() { }
+    Singleton( const Singleton& );
+    Singleton& operator=( Singleton& );
+   ~Singleton() { }
+    friend class SingletonDestroyer;
+  public:
+    static Singleton& getInstance();
+};
+  
+// Singleton.cpp
+#include "Singleton.h"
+  
+Singleton * Singleton::p_instance = 0;
+SingletonDestroyer Singleton::destroyer;
+  
+SingletonDestroyer::~SingletonDestroyer() {   
+    delete p_instance; 
+}
+void SingletonDestroyer::initialize( Singleton* p ) {
+    p_instance = p; 
+}
+Singleton& Singleton::getInstance() {
+    if(!p_instance)     {
+        p_instance = new Singleton();
+        destroyer.initialize( p_instance);     
+    }
+    return *p_instance;
+}
